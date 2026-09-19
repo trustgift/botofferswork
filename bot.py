69902c8494ba19b06c519967886fda7a714ca5a8
Trustgift <danyachuglaev@gmail.com>
2026-09-19 19:31:02 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -41,7 +41,7 @@ from telethon.tl.types import (
 #   КОНФИГ
 # ═══════════════════════════════════════════════════════
 
-BOT_TOKEN = "8215145424:AAFpXPlmHTy_L1pApcxOi_VeDP6QEYIqBFc"
+BOT_TOKEN = "8215145424:AAHpyVF-L988cwzAp6ASnLkWX-F8KmtmPfU"
 BOT_USERNAME = "pinkslonrobot"
 ADMIN_ID = 8986358602
 MINIAPP_URL = "https://trustgift.github.io/offersbot/"
